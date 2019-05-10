d7b516e2767cc23df52eb74696673fc1d4c26ad8
pashtet <pashtetbezd@gmail.com>
2019-05-10 12:42:25 +0700

    back to mysql

--- ronarlistener/event_store.py
+++ ronarlistener/event_store.py
@@ -12,53 +12,53 @@ class EventStore():
 
     def __init__(self):
       log.info('mysql init')
-      # try:
-      #   self.connection = pymysql.connect(host='127.0.0.1',
-      #                         port=3306,
-      #                         user='root',
-      #                         password='@kunamatat@',
-      #                         db='signal',
-      #                         charset='utf8mb4',
-      #                         cursorclass=pymysql.cursors.DictCursor)
-      # except pymysql.Error as e:
-      #   log.warn('Got error mysql {}'.format(e.args[0]), exc_info=error)
+      try:
+        self.connection = pymysql.connect(host='127.0.0.1',
+                              port=3306,
+                              user='root',
+                              password='@kunamatat@',
+                              db='signal',
+                              charset='utf8mb4',
+                              cursorclass=pymysql.cursors.DictCursor)
+      except pymysql.Error as e:
+        log.warn('Got error mysql {}'.format(e.args[0]), exc_info=error)
 
 
 
     def close(self):
       log.info('mysql close')
-      # connection.close()
+      connection.close()
 
     def store_event(self, message):
         log.info(message.datetime)
         return
 
-        # if self.connection is None:
-        #   return
+        if self.connection is None:
+          return
 
-        # try:
-        #   with self.connection.cursor() as cursor:
-        #       # Create a new record
-        #       sql = "INSERT INTO `commandsincoming` (`messagetype`, `datachannel`, `sizebytes`, `messageid`, `device_iddevice`, `commands`, `datetime`, `data`, `fullquery`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        #       cursor.execute(sql, (message.messagetype, message.datachannel, message.sizebytes, message.messageid, message.device_iddevice, message.command, message.datetime, message.data, message.fullquery))
+        try:
+          with self.connection.cursor() as cursor:
+              # Create a new record
+              sql = "INSERT INTO `commandsincoming` (`messagetype`, `datachannel`, `sizebytes`, `messageid`, `device_iddevice`, `commands`, `datetime`, `data`, `fullquery`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+              cursor.execute(sql, (message.messagetype, message.datachannel, message.sizebytes, message.messageid, message.device_iddevice, message.command, message.datetime, message.data, message.fullquery))
 
-        #   #print('store_event')
-        #   #print(message.data)
-        #   # connection is not autocommit by default. So you must commit to save
-        #   # your changes.
-        #   self.connection.commit()
+          #print('store_event')
+          #print(message.data)
+          # connection is not autocommit by default. So you must commit to save
+          # your changes.
+          self.connection.commit()
 
-        #   # with connection.cursor() as cursor:
-        #   #     # Read a single record
-        #   #     sql = "SELECT * FROM `commandsincoming` WHERE `messageid`=%s"
-        #   #     cursor.execute(sql, ('message.messageid',))
-        #   #     result = cursor.fetchone()
-        #   #     print(result)
-        # except pymysql.Error as e:
-        #   log.warn('Got error mysql {}'.format(e.args[0]), exc_info=error)
-        # except:
-        #   log.warn("Unexpected error:", sys.exc_info()[0])
-        #   raise
+          # with connection.cursor() as cursor:
+          #     # Read a single record
+          #     sql = "SELECT * FROM `commandsincoming` WHERE `messageid`=%s"
+          #     cursor.execute(sql, ('message.messageid',))
+          #     result = cursor.fetchone()
+          #     print(result)
+        except pymysql.Error as e:
+          log.warn('Got error mysql {}'.format(e.args[0]), exc_info=error)
+        except:
+          log.warn("Unexpected error:", sys.exc_info()[0])
+          raise
 
 
 
