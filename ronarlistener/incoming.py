864d5c3ef2401818b3e5dd82bc150ade212afde4
pashtet <pashtetbezd@gmail.com>
2019-08-04 02:52:49 +0500

    command test

--- ronarlistener/incoming.py
+++ ronarlistener/incoming.py
@@ -71,7 +71,7 @@ class Incoming():
         return self._message_type == b'\x13'
 
     def get_event(self):
-        return " ".join(["{:02x}".format(x).upper() for x in self._data]) if self._command[0] != b'\x24' else False
+        return False if self._command[0] == b'\x24' else " ".join(["{:02x}".format(x).upper() for x in self._command])
 
     def get_response(self):
         return self.get_response_from(
